163259b7db2681c9ab4a0f225a588b9b0ff2c914
Aravinda Rathnayake <aravindacloud@gmail.com>
2021-03-04 12:57:13 +0530

    Add initial environment setup with webpack configurations

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { appRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+
+import { RegisterComponent } from './register';
+import { AlertComponent } from './alert';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        RegisterComponent,
+        AlertComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { RegisterComponent } from './register';
+
+const routes: Routes = [
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
